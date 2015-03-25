90ac8d85099cb57c0789c2afea0f28d6357c9e0a
coltonlloyd <cjlloyd@eng.ucsd.edu>
2015-03-25 15:57:34 -0700

    Added function to create modification dictionary

--- ecolime/flat_files.py
+++ ecolime/flat_files.py
@@ -45,10 +45,11 @@ def get_reaction_to_modified_complex(generic=False):
             for i, cplx in enumerate(line[1:]):
                 for div in divalent_list:
                     if div in cplx:
-                        line[i+1] = cplx.replace(div, 'generic_divalent')
+                        cplx = cplx.replace(div, 'generic_divalent')
                 for mono in monovalent_list:
                     if mono in cplx:
-                        line[i+1] = cplx.replace(mono, 'generic_monovalent')
+                        cplx = cplx.replace(mono, 'generic_monovalent')
+                line[i+1] = cplx
         rxnToModCplxDict[line[0]] = line[1:]
     return rxnToModCplxDict
 
@@ -67,10 +68,12 @@ def get_reaction_matrix_dict():
             line[1] += '_p'
         elif line[2] == 'Extra-organism':
             line[1] += '_e'
+        elif line[2] == 'No_compartment':
+            line[1] += '_c'
         if line[0] not in ME_reaction_dict:
             ME_reaction_dict[line[0]] = []
         ME_reaction_dict[line[0]] += [[line[1], float(line[3])]]
-        return ME_reaction_dict
+    return ME_reaction_dict
 
 
 def get_reaction_info_dict():
@@ -87,7 +90,7 @@ def get_reaction_info_dict():
     return Reaction_info_dict
 
 
-def get_iJO_update_list():
+def get_full_iJO_update_list():
     ME_reactions = open('reactions.txt', 'r')
     iJO_update_list = []
     for line in ME_reactions:
@@ -96,3 +99,53 @@ def get_iJO_update_list():
             iJO_update_list.append(line[0])
     iJO_update_list = iJO_update_list[1:]
     return iJO_update_list
+
+
+def get_protein_modification_dict(generic=False):
+    """ Get dictionary of protein modifications to components for complexes
+    which don't act as metabolites in metabolic reaction.
+
+    Return: Modified_complex: [core_complex, modification_dict]
+
+    """
+    reaction_matrix = open('reaction_matrix.txt', 'r')
+    metlist = []
+    for line in reaction_matrix:
+        line = line.replace('\n', '')
+        line = line.split('\t')
+        line[1] = line[1].replace('DASH', '')
+        metlist.append(line[1])
+
+    enzMod = open('protein_modification.txt', 'r')
+    modification_dict = {}
+    for line in enzMod:
+        mod_dict = {}
+        line = line.rstrip('\tM_protein_recon\n')
+        line = line.rstrip('\t2011_Updated_E_recon\n')
+        line = re.split('\t| AND |', line)
+        # CPLX-7524_mod_mn2 acts as reactant in FE-S reactions and as enyzme
+        # for ALLTAMH
+        if line[0] not in metlist or line[0] == 'CPLX-7524_mod_mn2':
+            core_complex = line[1]
+            for term in line[2:]:
+                term = term.rstrip(')')
+                term = term.split('(')
+                try:
+                    stoich = float(term[1])
+                    mod = term[0]
+                except:
+                    stoich = 1
+                    mod = term[0]
+                if generic:
+                    if mod in divalent_list:
+                        line[0] = line[0].replace(mod, 'generic_divalent')
+                        mod = 'generic_divalent'
+                    if mod in monovalent_list:
+                        line[0] = line[0].replace(mod, 'generic_monovalent')
+                        mod = 'generic_monovalent'
+                mod = mod.replace('DASH', '') + '_c'
+                if mod not in mod_dict:
+                    mod_dict[mod] = 0
+                mod_dict[mod] += -stoich
+            modification_dict[line[0]] = [core_complex, mod_dict]
+    return modification_dict
