1927a64a2bd5c2fa787ad6af2b7909b2e7022ee9
coltonlloyd <cjlloyd@eng.ucsd.edu>
2017-02-21 11:55:23 -0800

    Fixes #9

--- ecolime/__init__.py
+++ ecolime/__init__.py
@@ -1,4 +1,3 @@
-import scripts
 from ecolime import (chaperones, DNA_replication, ecoli_k12, flat_files,
                      modifications, ribosome, transcription, translation,
                      translocation, tRNA_charging)
