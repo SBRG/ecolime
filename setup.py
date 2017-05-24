71a6e29a549d3ceedf489653c4d89ca0f11e86e1
coltonlloyd <cjlloyd@eng.ucsd.edu>
2017-05-24 14:58:49 -0700

    Specify xlrd package requirement

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='EcoliME',
       author='Ali Ebrahim and Colton Lloyd',
       author_email='minime_dev@googlegroups.com',
       url='https://github.com/SBRG/ecolime',
-      install_requires=['Biopython', 'setuptools', 'cobra<=0.5.11'],
+      install_requires=['Biopython', 'setuptools', 'cobra<=0.5.11', 'xlrd'],
       packages=find_packages(),
       package_data={'': ['building_data/*', 'me_models/*']},
       license='MIT')
