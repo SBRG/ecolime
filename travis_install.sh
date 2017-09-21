2e8cec42d99891a2034bb66ea780177889c60139
Colton Lloyd <coltonlloyd@Coltons-MBP.attlocal.net>
2017-09-21 13:28:09 -0700

    Travis test against complement Repo/Branch

--- travis_install.sh
+++ travis_install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -v
 
-if [ "${TRAVIS_BRANCH}" = "master" ]; then
-	pip install git+https://github.com/sbrg/cobrame.git@master
-else
-    pip install git+https://github.com/sbrg/cobrame.git@devel
-fi
+pip install git+https://github.com/${TRAVIS_REPO_SLUG%/*}/cobrame.git@${TRAVIS_BRANCH}
