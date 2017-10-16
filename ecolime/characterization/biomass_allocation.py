efd27a5a80ef9205e75129b98e669e475056f7ae
coltonlloyd <cjlloyd@eng.ucsd.edu>
2017-10-16 15:27:14 -0700

    Correct bugs in biomass allocation

--- ecolime/characterization/biomass_allocation.py
+++ ecolime/characterization/biomass_allocation.py
@@ -4,7 +4,7 @@ from sympy import Basic, Symbol
 from os.path import dirname, join, abspath
 
 import cobra.test
-from cobrame import dogma
+from cobrame.util import dogma
 
 ecoli_data_files_dir = dirname(abspath(__file__))
 
@@ -19,9 +19,13 @@ def get_biomass_composition(model, solution=None):
     if solution is None:
         solution = model.solution
     biomass_composition = defaultdict(float)
+
+    # Account for total biomass produced in protein_biomass_dilution reaction
+    protein_stoich = 1.
     for met, stoich in model._protein_biomass_dilution.metabolites.items():
-        if abs(stoich) > 1:
+        if abs(stoich) >= 1:
             protein_stoich = stoich
+
     biomass_composition['Protein'] = \
         solution.x_dict['protein_biomass_dilution'] * protein_stoich
     biomass_composition['tRNA'] = \
@@ -32,102 +36,57 @@ def get_biomass_composition(model, solution=None):
         solution.x_dict['ncRNA_biomass_dilution']
     biomass_composition['rRNA'] = \
         solution.x_dict['rRNA_biomass_dilution']
+    biomass_composition['DNA'] = \
+        solution.x_dict['DNA_biomass_dilution']
     biomass_composition['Other'] = \
         solution.x_dict['biomass_component_dilution']
 
     return biomass_composition
 
 
-def RNA_to_protein_ratio(model, solution=None):
-    composition = get_biomass_composition(solution=solution)
-    RNA_to_protein = (composition['mRNA'] + composition['tRNA'] +
+def rna_to_protein_ratio(model, solution=None):
+    if solution is None:
+        solution = model.solution
+
+    composition = get_biomass_composition(model, solution=solution)
+    rna_to_protein = (composition['mRNA'] + composition['tRNA'] +
                       composition['rRNA'] + composition['ncRNA']) / \
                      (composition['Protein'] +
                       composition['Unmodeled Protein'])
-    return RNA_to_protein
+    return rna_to_protein
 
 
-def get_RNA_fractions_dict(model, solution=None):
-    RNA_fractions = {}
-    composition = get_biomass_composition(solution=solution)
-
-    tRNA_to_RNA = (composition['tRNA']) / (
-        composition['mRNA'] + composition['tRNA'] + composition['rRNA'] +
-        composition['ncRNA'])
-    RNA_fractions['tRNA'] = tRNA_to_RNA
-
-    rRNA_to_RNA = (composition['rRNA']) / (
-        composition['mRNA'] + composition['tRNA'] + composition['rRNA'] +
-        composition['ncRNA'])
-    RNA_fractions['rRNA'] = rRNA_to_RNA
-
-    mRNA_to_RNA = (composition['mRNA']) / (
-        composition['mRNA'] + composition['tRNA'] + composition['rRNA'] +
-        composition['ncRNA'])
-    RNA_fractions['mRNA'] = mRNA_to_RNA
-
-    ncRNA_to_RNA = (composition['ncRNA']) / (
-        composition['mRNA'] + composition['tRNA'] + composition['rRNA'] +
-        composition['ncRNA'])
-    RNA_fractions['ncRNA'] = ncRNA_to_RNA
-
-    return RNA_fractions
-
-
-def get_membrane_composition(model, membrane='Inner_Membrane',
-                             solution=None):
-    if not solution:
+def get_rna_fractions_dict(model, solution=None):
+    if solution is None:
         solution = model.solution
 
-    composition = {}
+    rna_fractions = {}
+    composition = get_biomass_composition(model, solution=solution)
 
-    composition['Dummy_protein'] = solution.x_dict[
-        'SA_demand_dummy_protein_' + membrane]
-    composition['Modeled_protein'] = solution.x_dict[
-        'SA_demand_protein_' + membrane]
-    composition['Lipid'] = solution.x_dict[
-        'SA_demand_lipid_' + membrane]
+    trna_to_rna = (composition['tRNA']) / (
+        composition['mRNA'] + composition['tRNA'] + composition['rRNA'] +
+        composition['ncRNA'])
+    rna_fractions['tRNA'] = trna_to_rna
 
-    if membrane == 'Outer_Membrane':
-        composition['LPS'] = solution.x_dict[
-            'SA_demand_lps_' + membrane]
-        composition['Lipoprotein'] = solution.x_dict[
-            'SA_demand_lipoprotein_' + membrane]
-    return composition
+    rrna_to_rna = (composition['rRNA']) / (
+        composition['mRNA'] + composition['tRNA'] + composition['rRNA'] +
+        composition['ncRNA'])
+    rna_fractions['rRNA'] = rrna_to_rna
 
+    mrna_to_rna = (composition['mRNA']) / (
+        composition['mRNA'] + composition['tRNA'] + composition['rRNA'] +
+        composition['ncRNA'])
+    rna_fractions['mRNA'] = mrna_to_rna
 
-def get_membrane_protein(model, membrane='Inner_Membrane', solution=None):
-    if not solution:
-        solution = model.solution
+    ncrna_to_rna = (composition['ncRNA']) / (
+        composition['mRNA'] + composition['tRNA'] + composition['rRNA'] +
+        composition['ncRNA'])
+    rna_fractions['ncRNA'] = ncrna_to_rna
 
-    composition = defaultdict(float)
-
-    membrane_SA = model.metabolites.get_by_id('SA_protein_' + membrane)
-    for rxn in membrane_SA.reactions:
-        if membrane_SA in rxn.products:
-            coeff = rxn._metabolites[membrane_SA]
-            flux = coeff * solution.x_dict[rxn.id]
-            if flux != 0:
-                composition[
-                    rxn.id.replace('translocation_', '')] += flux
-        elif 'lipid_modification' in rxn.id:
-            coeff = rxn._metabolites[membrane_SA]
-            flux = coeff * solution.x_dict[rxn.id]
-            if flux != 0:
-                composition[rxn.id.split('_')[0]] += flux
-    composition['dummy'] = solution.x_dict[
-        'SA_demand_dummy_protein_' + membrane]
-    return composition
+    return rna_fractions
 
 
 def make_composition_piechart(model, kind='Biomass', solution=None):
-    try:
-        import brewer2mpl
-    except ImportError:
-        color_map = None
-    else:
-        color_map = brewer2mpl.wesanderson.Zissou.mpl_colormap
-
     try:
         import pandas
     except ImportError:
@@ -139,41 +98,14 @@ def make_composition_piechart(model, kind='Biomass', solution=None):
     summary = {}
     if kind == 'Biomass':
         summary['Biomass composition'] = \
-            get_biomass_composition(solution=solution)
+            get_biomass_composition(model, solution=solution)
         frame = pandas.DataFrame.from_dict(summary) / solution.f
-
-    elif kind == 'Inner_Membrane':
-        SA = solution.x_dict['SA_demand_Inner_Membrane']
-        summary['Inner_Membrane'] = \
-            model.get_membrane_composition(solution=solution, membrane=kind)
-        frame = pandas.DataFrame.from_dict(summary) / SA
-    elif kind == 'Outer_Membrane':
-        SA = solution.x_dict['SA_demand_Outer_Membrane']
-        summary['Outer_Membrane'] = \
-            model.get_membrane_composition(solution=solution, membrane=kind)
-        frame = pandas.DataFrame.from_dict(summary) / SA
-    elif kind == 'Protein_Inner_Membrane':
-        SA = solution.x_dict['SA_demand_protein_Inner_Membrane'] + \
-             solution.x_dict['SA_demand_dummy_protein_Inner_Membrane']
-        summary['Inner_Membrane_Protein'] = \
-            model.get_membrane_protein(solution=solution,
-                                      membrane='Inner_Membrane')
-        frame = pandas.DataFrame.from_dict(summary) / SA
-    elif kind == 'Protein_Outer_Membrane':
-        SA = solution.x_dict['SA_demand_protein_Outer_Membrane'] + \
-             solution.x_dict['SA_demand_dummy_protein_Outer_Membrane']
-        summary['Outer_Membrane_Protein'] = \
-            model.get_membrane_protein(solution=solution,
-                                      membrane='Outer_Membrane')
-        frame = pandas.DataFrame.from_dict(summary) / SA
-
     else:
         raise ('%s not a valid composition kind' % kind)
 
     print('Component sum =', frame.sum().values[0])
     frame = frame[frame > 1e-4].dropna()
-    return frame.plot(kind='pie', subplots=True, legend=None,
-                      colormap=color_map)
+    return frame.plot(kind='pie', subplots=True, legend=None)
 
 
 def compare_to_ijo_biomass(model, kind='amino_acid', solution=None):
@@ -197,7 +129,7 @@ def compare_to_ijo_biomass(model, kind='amino_acid', solution=None):
                 me_demand[aa] += r.translation_data.amino_acid_count[aa] * r.x
     elif kind == 'cofactors':
         for d in model.complex_data:
-            for mod, num in d.modifications.items():
+            for mod, num in d.subreactions.items():
                 me_demand[mod.replace('mod_', '')] += d.formation.x * num
     else:
         for met_id in biomass_rxn.metabolites:
@@ -209,8 +141,7 @@ def compare_to_ijo_biomass(model, kind='amino_acid', solution=None):
                         stoich = stoich.subs(mu, growth_rate)
                     me_demand[met_id.id] += r.x * stoich
 
-
-    compare ={}
+    compare = dict()
     compare['ME_gr_%.2f' % growth_rate] = me_demand.copy()
     compare['Measured'] = {}
     for met in me_demand:
@@ -220,7 +151,8 @@ def compare_to_ijo_biomass(model, kind='amino_acid', solution=None):
         else:
             continue
 
-        if ijo_met in biomass_rxn.metabolites and biomass_rxn._metabolites[ijo_met] < 0:
+        if ijo_met in biomass_rxn.metabolites and \
+                biomass_rxn._metabolites[ijo_met] < 0:
             compare['Measured'][met] = \
                 abs(model.solution.x_dict['biomass_dilution'] *
                     biomass_rxn._metabolites[ijo_met])
@@ -241,20 +173,21 @@ def get_protein_distribution(model, solution=None, groupby='COG'):
     transcription = model.get_translation_flux(solution=solution)
 
     if groupby == 'COG':
-        COG_df = pd.read_csv(fixpath('data/cogs_ecoli_mg1655.csv'))
-        COG_df = COG_df.set_index('locus')
+        cog_df = pd.read_csv(fixpath('data/cogs_ecoli_mg1655.csv'),
+                             encoding="ISO-8859-1")
+        cog_df = cog_df.set_index('locus')
         for protein, flux in transcription.items():
             protein_mass = model.translation_data.get_by_id(protein).mass
             if protein != 'dummy':
-                if protein in COG_df. index:
+                if protein in cog_df. index:
                     try:
-                        COG = COG_df.loc[protein, 'COG description'].values[0]
+                        cog = cog_df.loc[protein, 'COG description'].values[0]
                     except:
-                        COG = COG_df.loc[protein, 'COG description']
+                        cog = cog_df.loc[protein, 'COG description']
                 else:
-                    COG = 'No COG'
+                    cog = 'No COG'
 
-                protein_dict[COG] += protein_mass * flux
+                protein_dict[cog] += protein_mass * flux
             else:
                 protein_dict['dummy'] += protein_mass * flux
 
@@ -271,6 +204,6 @@ def get_protein_distribution(model, solution=None, groupby='COG'):
             if len(subsystem) > 0:
                 protein_dict['_'.join(subsystem)] = protein_mass * flux
     else:
-        raise('groupby flag is not "COG" or "Metabolic_Subsystem')
+        raise Exception('groupby flag is not "COG" or "Metabolic_Subsystem')
 
     return protein_dict
